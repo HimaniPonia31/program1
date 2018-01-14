7452615eb5aa62d6fb7e3d622ef2286ff0504119
HimaniPonia31 <35267711+HimaniPonia31@users.noreply.github.com>
2018-01-14 22:02:40 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	int i,n;
+	printf("enter a no. ");
+	scanf("%d",&n);
+	if(n>=1 && n<=100000)
+	{
+		if(n%2==0)
+		{
+		
+		printf("%d no is even",n);
+	}
+	else
+	{
+		printf("no is odd %d",n);
+	}
+}
+			
+		
+	else
+	{
+	
+		
+				printf("wrong no");
+	
+	 
+	
+}
+}
